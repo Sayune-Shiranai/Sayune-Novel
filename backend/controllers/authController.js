6e7e462d180f9976017c893bc8c5a4d56918a6a3
Sayune Shiranai <sayuneshiranai1404@gmail.com>
2025-10-13 01:53:32 +0700

    update login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -36,13 +36,54 @@ export async function register (req, res) {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await usersModel.create({ username, email, password: hashedPassword });
+    const user = await usersModel.create({ 
+      username, 
+      email, 
+      password: hashedPassword 
+    });
       return res.status(201).json({ 
         message: 'Tạo user thành công!', 
         user: { id: user.id, username: user.username, email: user.email }, 
       });
 }
 
-// export async function login (req, res) {
-//     const { username, password } = req.body;
-// }
+export async function login (req, res) {
+    const { username, password } = req.body;
+
+    if (!username)
+      return res.status(400).json({ field: "username", message: "Vui lòng nhập tên đăng nhập!" });
+
+    if (!password)
+        return res.status(400).json({ field: "password", message: "Vui lòng nhập mật khẩu!" });
+
+    const user = await usersModel.findOne({ where: { username } });
+    if (!user)
+      return res.status(400).json({ field: "username", message: "Tài khoản không tồn tại!" });
+
+    //example bcrypt.compare("123456", "$2b$10$gSY0P4HkHnNR3qDnPKhLVeFbf...vSVK/UZb4qB0E6") 
+    //bcrypt sẽ hash lại "123456" theo cùng cơ chế salt và so sánh với chuỗi hash trong DB.
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword)
+      return res.status(400).json({ field: "password", message: "Mật khẩu không đúng!" });
+
+        const accessToken = jwt.sign(
+          { username: user.username, role: user.role },
+          { expiresIn: "1m" }
+        );
+
+        const refreshToken = jwt.sign(
+          { username: user.username, role: user.role },
+          { expiresIn: "7d" }
+        );
+
+    return res.status(200).json({ 
+      message: "Đăng nhập thành công!", 
+      user: { 
+        id: user.id, 
+        username: user.username, 
+        email: user.email 
+      }, 
+      accessToken,
+      refreshToken
+    });
+}
