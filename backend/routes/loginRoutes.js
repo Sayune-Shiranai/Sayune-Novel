4bedc2cd1c228627a797930ef01d4ea7bc81b682
Sayune Shiranai <sayuneshiranai1404@gmail.com>
2025-11-20 00:28:02 +0700

    fix login

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { login } from "../controllers/loginController.js";
 
 const router = express.Router();
-router.post("/", login);
+router.get("/", login);
 
 
 export default router;
