fe87e440f6a8ebd5315df5d067074e455cb1df77
Sayune Shiranai <sayuneshiranai1404@gmail.com>
2025-12-05 02:58:13 +0700

    update comment volume

--- backend/routes/dashboard/BookRoutes.js
+++ backend/routes/dashboard/BookRoutes.js
@@ -1,11 +1,7 @@
 import express from "express";
 import upload from "../../middleware/upload.js";
-// const multer = require("multer");
-// // const storage = multer.memoryStorage();
-// const upload = multer({ dest: 'media/books_images' });
 
 import {
-  // getOneBook,
   GetPaged,
   createBook,
   updateBook,
@@ -16,11 +12,11 @@ import {
 
 const router = express.Router();
 
-router.get("/", GetPaged); //dashboard/book
-router.post("/create", upload.single("img"), createBook);      //dashboard/book/create
-router.post("/update/:slug",upload.single("img"), updateBook); // /dashboard/book/update/:slug
-router.delete("/delete/:id", deleteBook);     // /dashboard/book/delete/:id
-router.post("/approve/:id", approveBook);
-router.post("/reject/:id", rejectBook);
+router.get("/", GetPaged);  //dashboard/book
+router.post("/create", upload.single("img"), createBook); //dashboard/book/create
+router.post("/update/:slug",upload.single("img"), updateBook);  //dashboard/book/update/:slug
+router.delete("/delete/:id", deleteBook); //dashboard/book/delete/:id
+router.post("/approve/:id", approveBook); //dashboard/book/approve/:id
+router.post("/reject/:id", rejectBook); //dashboard/book/reject/:id
 
 export default router;
--- backend/routes/dashboard/VolumeRoutes.js
+++ backend/routes/dashboard/VolumeRoutes.js
@@ -8,11 +8,11 @@ import {
 } from "../../controllers/volumeController";
 
 const router = express.Router();
-router.get("/", GetPaged); //dashboard/book/:slug
-router.get("/create", createVolume) //dashboard/book/:slug/create
-router.post("/delete/:id", deleteVolume);
-router.post("/approve/:id", approveVolume);
-router.post("/reject/:id", rejectVolume);
+router.get("/", GetPaged);  //dashboard/book/:slug
+router.get("/create", createVolume);    //dashboard/book/:slug/create
+router.post("/delete/:id", deleteVolume);   //dashboard/book/:slug/create/:id
+router.post("/approve/:id", approveVolume); //dashboard/book/:slug/approve/:id
+router.post("/reject/:id", rejectVolume);   //dashboard/book/:slug/reject/:id
 
 
 export default router;
